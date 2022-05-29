e9679704d04ed61381b27856b1b145801467cc6a
VeritosHD <veritoshd@gmail.com>
2022-05-29 19:14:20 +0500

    failed commitment

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.test
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
+        title = "Diabilities"
     }
+
+
+
+
+
+
 }
 
