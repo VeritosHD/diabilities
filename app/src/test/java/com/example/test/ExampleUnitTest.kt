0237e8439ffce1cbfcaeadf89b5c5b205b3a6c3b
VeritosHD <veritoshd@gmail.com>
2022-05-29 12:40:46 +0500

    fourth commitment

--- app/src/test/java/com/example/test/ExampleUnitTest.kt
+++ app/src/test/java/com/example/test/ExampleUnitTest.kt
@@ -1,6 +1,5 @@
 package com.example.test
 
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
@@ -11,9 +10,7 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun generates_number(){
-        val dice = Dice(6)
-        val rollResult = dice.roll()
-        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+
     }
 
 }
